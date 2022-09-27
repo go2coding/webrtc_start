1f8966f3253d4f28a8d0a9c1821f36a1fc77fa46
dolar <191076013@qq.com>
2022-09-27 02:53:16 +0000

    localDescription ice

---
+++ demo4_RTCPeerConnection/js/main2.js
@@ -0,0 +1,158 @@
+/*
+ *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+
+'use strict';
+
+const callButton = document.getElementById('callButton');
+
+callButton.disabled = true;
+
+callButton.addEventListener('click', call);
+
+let startTime;
+const localVideo = document.getElementById('localVideo');
+const remoteVideo = document.getElementById('remoteVideo');
+
+
+
+let localStream;
+let pc1;
+let pc2;
+
+let num = 1;
+
+
+function getName(pc) {
+    return (pc === pc1) ? 'pc1' : 'pc2';
+}
+
+//开启音视频源
+async function start() {
+    console.log('Requesting local stream');
+
+    try {
+        //捕获摄像头和麦克风的流，放到localVideo中
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+        console.log('Received local stream');
+        localVideo.srcObject = stream;
+        localStream = stream;
+        callButton.disabled = false;
+    } catch (e) {
+        alert(`getUserMedia() error: ${e.name}`);
+    }
+}
+
+start();
+
+//拨打，建立连接
+async function call() {
+    callButton.disabled = true;
+    console.log('Starting call');
+    startTime = window.performance.now();
+
+    const configuration = {};
+    console.log('RTCPeerConnection configuration:', configuration);
+
+    //源连接，
+    pc1 = new RTCPeerConnection(configuration);
+
+    //把localStream的音视频，放到源中
+    localStream.getTracks().forEach(track => pc1.addTrack(track, localStream));
+
+    //目标
+    pc2 = new RTCPeerConnection(configuration);
+    //等待源发来的流
+    pc2.addEventListener('track', gotRemoteStream);
+
+    //当ice准备好后，加到目标源中
+    pc1.addEventListener('icecandidate', e => onIceCandidate(pc2, e));
+
+
+
+
+
+    try {
+        console.log('pc1 createOffer start');
+
+        const offerOptions = {
+            offerToReceiveAudio: 1,
+            offerToReceiveVideo: 1
+        };
+
+        //创建和设置连接描述
+        const desc_pc1 = await pc1.createOffer(offerOptions);
+
+        console.log("desc_pc1 :");
+        console.log(desc_pc1);
+        await pc1.setLocalDescription(desc_pc1);
+
+
+
+    } catch (e) {
+        onCreateSessionDescriptionError(e);
+    }
+}
+
+
+
+
+function onCreateSessionDescriptionError(error) {
+    console.log(`Failed to create session description: ${error.toString()}`);
+}
+
+
+function onSetLocalSuccess(pc) {
+    console.log(`${getName(pc)} setLocalDescription complete`);
+}
+
+function onSetRemoteSuccess(pc) {
+    console.log(`${getName(pc)} setRemoteDescription complete`);
+}
+
+function onSetSessionDescriptionError(error) {
+    console.log(`Failed to set session description: ${error.toString()}`);
+}
+
+function gotRemoteStream(e) {
+    if (remoteVideo.srcObject !== e.streams[0]) {
+        remoteVideo.srcObject = e.streams[0];
+        console.log('pc2 received remote stream');
+    }
+}
+
+
+
+async function onIceCandidate(pc, event) {
+
+    if (event.candidate === null) {
+
+        const desc_pc1 = pc1.localDescription;
+
+        console.log("desc_pc1 candidate == null:");
+        console.log(pc1.localDescription);
+
+        await pc2.setRemoteDescription(desc_pc1);
+        const desc_pc2 = await pc2.createAnswer();
+
+        console.log("answer desc_pc2 :");
+        console.log(desc_pc2);
+        await pc2.setLocalDescription(desc_pc2);
+
+        await pc1.setRemoteDescription(desc_pc2);
+    }
+
+}
+
+function onAddIceCandidateSuccess(pc) {
+    console.log(`${getName(pc)} addIceCandidate success`);
+}
+
+function onAddIceCandidateError(pc, error) {
+    console.log(`${getName(pc)} failed to add ICE Candidate: ${error.toString()}`);
+}
+
